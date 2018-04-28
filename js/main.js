2d3cb95cd994b5d8c8374478a4d9da308abf4b58
William <williamandrewshilton@gmail.com>
2018-04-28 11:34:27 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,57 @@
+// console.log("connected");
+
+// //primitive data types
+//
+// //1. NUMBERS
+//     8
+//     100
+//     5.379
+// //2. STRINGS
+//     "connected"
+//     "this is a string"
+//     "D"
+//     ""
+//     "7"
+//     "131"
+// //3. BOOLEANS
+//     true
+//     false
+// //4. and 5.
+//     null
+//     undefined //error
+
+
+//variables - you can store anything inside of a variable in javascript.
+
+
+// console.log("connected")
+// document.write("This writes content to the HTML webpage")
+
+// prompt()
+
+//DOT NOTATION
+
+// alert("This creates a popup");
+// prompt("This is a prompt. How are you today?");
+
+// var firstName = prompt("What is your first name?");
+// var lastName = prompt("What is your last name?");
+//
+// document.write("Hello, " + firstName + " " + lastName + "!");
+
+
+
+
+//LOGICAL OPERATORS
+// && //BOTH SIDES MUST BE TRUE
+// || //ONE SIDE MUST BE TRUE
+// ! //RETURNS THE BOOLEAN OPPOSITE
+
+
+
+//STRING PROPERTIES
+    //.length
+    //[] :index starts at 0. eg. 0,1,2,3...
+
+//LOOPS - FOR LOOPS
+  //ALLOW US TO RUN BLOCKS OF CODE MULTIPLE TIMES
